fdf60d815963d5647eb6af83008a4fd621ffee13
Roy Clarkson <rclarkson@vmware.com>
2013-02-06 23:57:55 -0500

    created projects

---
+++ client/PizzaShop/PizzaShop/RCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RCAppDelegate.h
+//  PizzaShop
+//
+//  Created by Roy Clarkson on 2/6/13.
+//  Copyright (c) 2013 Roy Clarkson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ client/PizzaShop/PizzaShop/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PizzaShop
+//
+//  Created by Roy Clarkson on 2/6/13.
+//  Copyright (c) 2013 Roy Clarkson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RCAppDelegate class]));
+    }
+}
